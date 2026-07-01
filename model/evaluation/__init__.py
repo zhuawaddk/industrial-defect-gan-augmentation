f6a7d2c47a7e4e1ac1a2fea0d144eda6f077612f
zhuawaddk <2780085675@qq.com>
2026-07-01 15:36:19 +0800

    Delete model directory

--- model/evaluation/__init__.py
+++
@@ -1,5 +0,0 @@
-from .evaluator import Evaluator, FIDScore, InceptionScore
-from .anomaly_detection_evaluator import AnomalyDetectionEvaluator
-from .pps import PhysicalPlausibilityScore
-from .visualizer import Visualizer, create_default_visualizations
-from .ablation import AblationRunner, AblationConfig, ABLATION_CONFIGS, run_hyperparameter_sensitivity
